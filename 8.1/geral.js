4443f9a43c4b3807bac18802b8f2ad5ccb446f4e
JVictor Baptista <dixxbaptista@gmail.com>
2021-06-23 21:52:01 -0300

    bloco 8.1

---
+++ 8.1/geral.js
@@ -0,0 +1,52 @@
+// const repeat = (number, action) => {
+//   for (let count = 0; count <= number; count += 1) {
+//     action(count);
+//   }
+// };
+
+// repeat(5, console.table);
+
+// const repeat = (number, action) => {
+//   for (let count = 0; count <= number; count += 1) {
+//     action(count);
+//   }
+// };
+
+// repeat(3, (number) => {
+//   if (number % 2 === 0) {
+//     console.log(number, 'is even');
+//   }
+// });
+
+// const repeat = (number, action) => {
+//   for (let count = 0; count <= number; count += 1) {
+//     action(count);
+//   }
+// };
+
+// const isEven = (number) => {
+//   if (number % 2 === 0) {
+//     console.log(number, 'is even');
+//   }
+// };
+
+// const isOdd = (number) => {
+//   if ((number % 2) > 0) {
+//     console.log(number, 'is odd');
+//   }
+// };
+
+// repeat(3, isEven); // Testa quais números serão pares;
+// repeat(3, isOdd); // Testa quais números serão ímpares;
+
+// const sum = (num1, num2) => num1 + num2;
+
+// const sub = (num1, num2) => num1 - num2;
+
+// const multi = (num1, num2) => num1 * num2;
+
+// const div = (num1, num2) => num1 / num2;
+
+// const calculate = (func,num1,num2) => func(num1,num2)
+
+// console.log(calculate(div,50,50));
